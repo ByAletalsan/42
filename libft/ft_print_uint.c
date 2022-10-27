8887a8f2eb6b2456feee9e163fe8649ff256cac3
ByAletalsan <byaletalsan@gmail.com>
2022-10-27 11:44:29 +0200

    update with printf

---
+++ libft/ft_print_uint.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_uint.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atalaver <atalaver@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/08 10:27:17 by atalaver          #+#    #+#             */
+/*   Updated: 2022/10/27 11:40:57 by atalaver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_cntchr(unsigned int n, t_bonus *b)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0 && (b->limit > 0 || b->punto == 0))
+		i++;
+	while (n != 0)
+	{
+		i++;
+		n /= 10;
+	}
+	return (i);
+}
+
+static char	*ft_uitoa(unsigned int n, t_bonus *b)
+{
+	char	*s;
+	int		len;
+	int		i;
+
+	len = ft_cntchr(n, b);
+	i = len;
+	s = (char *)malloc(len + 1);
+	if (!s)
+		return (0);
+	s[i] = '\0';
+	while (i != 0)
+	{
+		s[i - 1] = (n % 10) + '0';
+		n /= 10;
+		i--;
+	}
+	return (s);
+}
+
+static int	ft_print_spaces_uint(t_bonus *b, int p)
+{
+	int	i;
+
+	i = 0;
+	if (b->menos == 0 && p == 0)
+	{
+		while (i < (b->width - b->limit))
+		{
+			if (b->cero == 0 || b->punto == 1)
+				ft_print_char(' ');
+			else
+				ft_print_char('0');
+			i++;
+		}
+	}
+	if (b->menos == 1 && p == 1)
+	{
+		while (i < (b->width - b->limit))
+		{
+			ft_print_char(' ');
+			i++;
+		}
+	}
+	return (i);
+}
+
+static int	ft_print_ceros_uint(t_bonus *b, unsigned int n)
+{
+	int	i;
+
+	i = 0;
+	while (b->punto == 1 && i < (b->limit - ft_cntchr(n, b)))
+	{
+		ft_print_char('0');
+		i++;
+	}
+	return (i);
+}
+
+int	ft_print_uint(unsigned int n, t_bonus *b)
+{
+	char	*s;
+	int		r;
+
+	r = 0;
+	if (b->limit < ft_cntchr(n, b))
+		b->limit = ft_cntchr(n, b);
+	r += ft_print_spaces_uint(b, 0);
+	r += ft_print_ceros_uint(b, n);
+	s = ft_uitoa(n, b);
+	ft_putstr_fd(s, 1);
+	r += ft_strlen(s);
+	r += ft_print_spaces_uint(b, 1);
+	free(s);
+	return (r);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: atalaver <atalaver@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:14:39 by atalaver          #+#    #+#             */
-/*   Updated: 2022/09/23 20:20:30 by atalaver         ###   ########.fr       */
+/*   Updated: 2022/10/27 11:38:46 by atalaver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
 typedef struct s_list
 {
@@ -64,5 +65,35 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+//Printf
+typedef struct s_bonus
+{
+	int				menos;
+	int				cero;
+	int				punto;
+	int				width;
+	int				almoadilla;
+	int				espacio;
+	int				mas;
+	int				limit;
+}					t_bonus;
+
+int		ft_printf(char const *s, ...);
+int		ft_print_char(char c);
+int		ft_print_char_bonus(t_bonus *b, char c);
+int		ft_print_string(char *s, t_bonus *b);
+int		ft_print_int(int n, t_bonus *b);
+int		ft_print_memory(unsigned long long n, t_bonus *b);
+int		ft_print_uint(unsigned int n, t_bonus *b);
+int		ft_print_hexa(unsigned int n, char format, t_bonus *b);
+void	ft_startbonus(t_bonus *b);
+void	ft_add_bonus(t_bonus *b, const char *s, int *i);
+int		ft_print_space_memory(t_bonus *b, unsigned long long n, int p);
+int		ft_print_ceros(t_bonus *b, int n);
+int		ft_print_spaces_int(t_bonus *b, int p);
+int		ft_print_ceros_hex(t_bonus *b, unsigned int n);
+int		ft_memlen(unsigned int n, t_bonus *b);
+int		ft_print_space(t_bonus *b, int p);
 
 #endif
